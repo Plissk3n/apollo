b46b13c07e4016f630a0174878d09f2a0895562c
Plissk3n <34848509+Plissk3n@users.noreply.github.com>
2018-07-11 18:47:06 +0200

    Update Master.ino

--- espnow/Master.ino
+++ espnow/Master.ino
@@ -167,7 +167,38 @@ void manageSlave() {
   }
 }
 
+void sendSingleData() {
+      Serial.print("Sending: ");
+      Serial.println(data);
+   
+    esp_err_t result = esp_now_send(peer_addr, &data, sizeof(data));
+    
+   
+   //status-report of 
+   Serial.print("Send Status: ");
+    if (result == ESP_OK) {
+      Serial.println("Success");
+    } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+      // How did we get so far!!
+      Serial.println("ESPNOW not Init.");
+    } else if (result == ESP_ERR_ESPNOW_ARG) {
+      Serial.println("Invalid Argument");
+    } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+      Serial.println("Internal Error");
+    } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+      Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+    } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+      Serial.println("Peer not found.");
+    } else {
+      Serial.println("Not sure what happened");
+    }
+    delay(100);
+  }
+}
 
+
+
+/*
 uint8_t data = 0;
 // send data
 void sendData() {
@@ -199,6 +230,8 @@ void sendData() {
     delay(100);
   }
 }
+*/
+
 
 // callback when data is sent from Master to Slave
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
